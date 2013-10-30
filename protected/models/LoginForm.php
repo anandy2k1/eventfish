b430db841f42a818c85baa21dcd143ccfe217e7d
anandy2k1 <anandy2k1@gmail.com>
2013-10-30 14:29:20 +0530

    revert facebookcode

--- protected/models/LoginForm.php
+++ protected/models/LoginForm.php
@@ -1,80 +1,70 @@
 <?php
+
 /**
- * class LoginForm
- * @author Igor Ivanović
- * Main login system 
+ * LoginForm class.
+ * LoginForm is the data structure for keeping
+ * user login form data. It is used by the 'login' action of 'SiteController'.
  */
-class LoginForm extends CFormModel
-{
+class LoginForm extends CFormModel {
 
     public $username;
     public $password;
-    public $_identity;
+    public $rememberMe;
+    private $_identity;
 
     /**
-     * Model rules
-     * @return type 
+     * Declares the validation rules.
+     * The rules state that username and password are required,
+     * and password needs to be authenticated.
      */
-    public function rules() 
-    {
+    public function rules() {
         return array(
-            array("username", "required", "message" => "Korisničko ime je obavezno."),
-            array("password", "required", "message" => "Lozinka je obavezna."),
-            array("password", "authenticate"),            
+            // username and password are required
+            array('username, password', 'required'),
+            // rememberMe needs to be a boolean
+            array('rememberMe', 'boolean'),
+            // password needs to be authenticated
+            array('password', 'authenticate'),
         );
     }
-  
+
+    /**
+     * Declares attribute labels.
+     */
+    public function attributeLabels() {
+        return array(
+            'rememberMe' => 'Remember me next time',
+        );
+    }
+
     /**
      * Authenticates the password.
      * This is the 'authenticate' validator as declared in rules().
      */
-    public function authenticate() 
-    {
-        $this->_identity = new UserIdentity($this->username,$this->password);
-        
-        if( $this->_identity->authenticate() !== UserIdentity::ERROR_NONE ) 
-        {
-            $this->addError( 'username', Yii::t('Web', 'Kriva kombinacija lozinke i korisničkog imena.') );
+    public function authenticate($attribute, $params) {
+        if (!$this->hasErrors()) {
+            $this->_identity = new UserIdentity($this->username, $this->password);
+            if (!$this->_identity->authenticate())
+                $this->addError('password', 'Incorrect username or password.');
         }
     }
-    
-    /**
-     * Model attribute labels
-     * @return type 
-     */
-    public function attributeLabels() 
-    {
-        return array(
-            "username"              => Yii::t("lang", "Korisničko ime"),
-            "password"              => Yii::t("lang", "Lozinka"),
-        );
-    }    
-    
 
     /**
      * Logs in the user using the given username and password in the model.
      * @return boolean whether login is successful
      */
-    public function login() 
-    {
-        
-        if($this->_identity === NULL) 
-        {
+    public function login() {
+        if ($this->_identity === null) {
             $this->_identity = new UserIdentity($this->username, $this->password);
             $this->_identity->authenticate();
         }
-           
-        if($this->_identity->errorCode === UserIdentity::ERROR_NONE) 
-        {
-            Yii::app()->user->login($this->_identity, NULL);
-            return TRUE; 
-        } 
-        else 
-        {
-            return FALSE;
+        if ($this->_identity->errorCode === UserIdentity::ERROR_NONE) {
+            $duration = $this->rememberMe ? 3600 * 24 * 30 : 0; // 30 days
+            Yii::app()->user->login($this->_identity, $duration);
+            return true;
         }
-        
+        else
+            return false;
     }
-    
-    
+
 }
